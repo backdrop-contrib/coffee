37c8799ff7d755e0d2ad7f1767abbd567da73004
Maarten Verbaarschot <maarten.verbaarschot@me.com>
2012-02-02 22:55:57 +0100

    overall code improvements

--- js/coffee.js
+++ js/coffee.js
@@ -1,44 +1,45 @@
 (function ($) {
 
+Drupal.coffee = Drupal.coffee || {};
+
 Drupal.behaviors.coffee = {
   attach: function () {
     $('body').once('coffee', function () {
 
       var body = $(this);
 
-      Drupal.coffee.bg.appendTo(body).hide();
+      // use a background element to make the form closable by clicking outside it
+      Drupal.coffee.bg.appendTo(body);
       
-      Drupal.coffee.form
-        .append(Drupal.coffee.label)
-        .append(Drupal.coffee.field)
-        .append(Drupal.coffee.results)
-        .wrapInner('<div id="coffee-form-inner" />')
-        .appendTo(body)
-        .hide();
+      // add an inner div for validation and styling
+      Drupal.coffee.form.wrapInner('<div id="coffee-form-inner" />').appendTo(body);
+      
+      // hide the form until alt+D is pressed
+      Drupal.coffee.hide();
 
       $(document).keydown(function (event) {
         
         var activeElement = $(document.activeElement);
 
-        // Show the form with alt + D. Use 2 keycodes as 'D' can be uppercase or lowercase.
-        if (!Drupal.coffee.form.is(':visible') && event.altKey === true && (event.keyCode === 68 || event.keyCode === 206)) {
-          Drupal.coffee.open();
+        // show the form with alt+D. 2 keycodes as 'D' can be upper- or lowercase
+        if ( !Drupal.coffee.form.is(':visible') && event.altKey === true && (event.keyCode === 68 || event.keyCode === 206) ) {
+          Drupal.coffee.show();
           event.preventDefault();
         }
 
-        // Close the form with esc or alt + D.
-        else if (Drupal.coffee.form.is(':visible') && (event.keyCode === 27 || (event.altKey === true && (event.keyCode === 68 || event.keyCode === 206)))) {
-          Drupal.coffee.close();
+        // hide the form with esc or alt+D
+        else if ( Drupal.coffee.form.is(':visible') && (event.keyCode === 27 || (event.altKey === true && (event.keyCode === 68 || event.keyCode === 206))) ) {
+          Drupal.coffee.hide();
           event.preventDefault();
         }
 
-        // Use the arrow up/down keys to navigate trough the results.
-        else if (Drupal.coffee.form.is(':visible') && Drupal.coffee.results.children().length && (event.keyCode === 38 || event.keyCode === 40)) {
-          Drupal.coffee.move(event.keyCode === 38 ? 'prev' : 'next', activeElement);
+        // use the arrow up/down keys to navigate trough the results
+        else if ( Drupal.coffee.form.is(':visible') && Drupal.coffee.results.children().length && (event.keyCode === 38 || event.keyCode === 40) ) {
+          Drupal.coffee.moveFocus(event.keyCode === 38 ? 'prev' : 'next', activeElement);
           event.preventDefault();
         }
 
-        // Enter key handling for the search field: redirect to the first result if there are any.
+        // enter key handling for the search field: follow the first result link
         else if (Drupal.coffee.form.is(':visible') && event.keyCode === 13 && activeElement[0] === Drupal.coffee.field[0]) {
           if (Drupal.coffee.results.children().length) {
             Drupal.coffee.results.find('a:first').addClass('trigger').click();
@@ -48,14 +49,17 @@ Drupal.behaviors.coffee = {
 
       });
 
+
       // .live() is deprecated ==>> convert to .on() when Drupal gets jQuery 1.7+ (http://api.jquery.com/live/)
-      // Remove the fake focus class once actual focus is used.
+      
+      // remove the fake focus class once actual focus is used
       $('#coffee-results a').live('focus', function () {
         Drupal.coffee.results.find('.focus').removeClass('focus');
       })
-      // We close the form explicitly after following a link as pages aren't reloaded when the overlay module is used.
+      
+      // hide the form explicitly after following a link as pages aren't reloaded when the overlay is used
       .live('click', function () {
-        Drupal.coffee.close();
+        Drupal.coffee.hide();
       });
       
       // simulate native click behavior when using .trigger()
@@ -67,85 +71,79 @@ Drupal.behaviors.coffee = {
   }
 };
 
-Drupal.coffee = Drupal.coffee || {};
-
-Drupal.coffee.label = $('<label for="coffee-q" class="element-invisible" />').text(Drupal.t('Query'));
-
-Drupal.coffee.field = $('<input id="coffee-q" type="text" autocomplete="off" />').keyup(function () {
-  Drupal.coffee.resultsPlaceholder.empty();
-
-  $.getJSON(Drupal.settings.basePath + '?q=admin/coffee/result/' + Drupal.coffee.field.val(), function (data) {
-    if (data) {
-      $.each(data, function (key, value) {
-        var description = $('<small class="description" />').text(value.path);
-        $('<a />').text(value.title)
-          .attr('href', Drupal.settings.basePath + value.path)
-          .append(description)
-          .appendTo(Drupal.coffee.resultsPlaceholder)
-          .wrap('<li />');
-      });
-
-      // Highlight the first result as if it were focused, as a visual hint for
-      // what will happen when the enter key is used in the search field.
-      Drupal.coffee.results.html(Drupal.coffee.resultsPlaceholder.children()).find('a:first').addClass('focus');
-    }
-  });
-});
-
-// Instead of appending results one by one, we put them in a placeholder element
-// first and then append them all at once to prevent flickering while typing.
-
-Drupal.coffee.results = $('<ol id="coffee-results" />');
-
-Drupal.coffee.resultsPlaceholder = $('<ol />');
-
-Drupal.coffee.form = $('<form id="coffee-form" />');
-
-Drupal.coffee.bg = $('<div id="coffee-bg" />').click(function () {
-  Drupal.coffee.close();
-});
-
-Drupal.coffee.open = function () {
+Drupal.coffee.show = function () {
   Drupal.coffee.form.show();
   Drupal.coffee.bg.show();
   Drupal.coffee.field.focus();
 };
 
-Drupal.coffee.close = function () {
+Drupal.coffee.hide = function () {
   Drupal.coffee.field.val('');
   Drupal.coffee.results.empty();
   Drupal.coffee.form.hide();
   Drupal.coffee.bg.hide();
 };
 
-/**
- * Move the focus to the previous or next search result.
- *
- * @param direction
- *   "prev" or "next".
- * @param activeElement
- *   jQuery object for the element to use as starting point.
- */
-Drupal.coffee.move = function (direction, activeElement) {
+Drupal.coffee.moveFocus = function (direction, activeElement) {
   
-  // From the search field: jump to the first (next) or last (prev) result.
-  // Skip the first result if it already has the fake focus class.
+  // from the search field: jump to the first (next) or last (prev) result.
+  // skip the first result if it already has the fake focus class.
   if (activeElement[0] === Drupal.coffee.field[0]) {
     Drupal.coffee.results.find( direction === 'prev' ? 'a:last' : (Drupal.coffee.results.find('a:first').hasClass('focus') ? 'li:nth-child(2) a' : 'a:first') ).focus();
   }
   
-  // Jump to the last result if prev is used on the first, and the other way around.
+  // jump to the last result if prev is used on the first, and the other way around.
   else if (activeElement[0] === Drupal.coffee.results.find(direction === 'prev' ? 'a:first' : 'a:last')[0]) {
     Drupal.coffee.results.find( direction === 'prev' ? 'a:last' : 'a:first' ).focus();
   }
   
-  // Assuming we're on a result link.
+  // assuming we're on a result link
   else if (direction === 'prev') {
     activeElement.parent().prev().find('a').focus();
-  }
-  else {
+  } else {
     activeElement.parent().next().find('a').focus();
   }
 };
+
+Drupal.coffee.bg = $('<div id="coffee-bg" />')
+  .click(function () {
+    Drupal.coffee.hide();
+  });
+
+Drupal.coffee.form = $('<form id="coffee-form" />');
+
+Drupal.coffee.label = $('<label for="coffee-q" class="element-invisible" />')
+  .appendTo(Drupal.coffee.form)
+  .text(Drupal.t('Query'));
+
+Drupal.coffee.field = $('<input id="coffee-q" type="text" autocomplete="off" />')
+  .appendTo(Drupal.coffee.form)
+  .keyup(function () {
+    Drupal.coffee.resultsPlaceholder.empty();
+
+    $.getJSON(Drupal.settings.basePath + '?q=admin/coffee/result/' + Drupal.coffee.field.val(), function (data) {
+      if (data) {
+        $.each(data, function (key, value) {
+          var description = $('<small class="description" />').text(value.path);
+          $('<a />').text(value.title)
+            .attr('href', Drupal.settings.basePath + value.path)
+            .append(description)
+            .appendTo(Drupal.coffee.resultsPlaceholder)
+            .wrap('<li />');
+        });
+
+        // highlight the first result as if it were focused, as a visual hint for
+        // what will happen when the enter key is used in the search field.
+        Drupal.coffee.results.html(Drupal.coffee.resultsPlaceholder.children()).find('a:first').addClass('focus');
+      }
+    });
+  });
+
+// instead of appending results one by one, we put them in a placeholder element
+// first and then append them all at once to prevent flickering while typing.
+
+Drupal.coffee.results = $('<ol id="coffee-results" />').appendTo(Drupal.coffee.form);
+
+Drupal.coffee.resultsPlaceholder = $('<ol />');
 
 }(jQuery));
