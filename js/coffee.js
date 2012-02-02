1e1faa0e9237f9c8696a0157dfdb849a807f69ec
Maarten Verbaarschot <maarten.verbaarschot@me.com>
2012-02-02 15:15:03 +0100

    further cleaning

--- js/coffee.js
+++ js/coffee.js
@@ -1,87 +1,5 @@
 (function ($) {
-
-  Drupal.coffee = Drupal.coffee || {};
-
-  Drupal.coffee.label = $('<label for="coffee-q" class="element-invisible" />').text(Drupal.t('Query'));
-
-  Drupal.coffee.results = $('<ol id="coffee-results" />');
-
-  // Instead of appending results one by one, we put them in a placeholder element
-  // first and then append them all at once to prevent flickering while typing.
-
-  Drupal.coffee.resultsPlaceholder = $('<ol />');
-
-  Drupal.coffee.form = $('<form id="coffee-form" />');
-
-  Drupal.coffee.bg = $('<div id="coffee-bg" />').click(function () {
-    Drupal.coffee.close();
-  });
-
-  Drupal.coffee.field = $('<input id="coffee-q" type="text" autocomplete="off" />').keyup(function () {
-    Drupal.coffee.resultsPlaceholder.empty();
-
-    $.getJSON(Drupal.settings.basePath + 'admin/coffee/result/' + Drupal.coffee.field.val(), function (data) {
-      if (data) {
-        $.each(data, function (key, value) {
-          var description = $('<small class="description" />').text(value.path);
-          $('<a />').text(value.title)
-            .attr('href', Drupal.settings.basePath + value.path)
-            .append(description)
-            .appendTo(Drupal.coffee.resultsPlaceholder)
-            .wrap('<li />');
-        });
-
-        // Highlight the first result as if it were focused, as a visual hint for
-        // what will happen when the enter key is used in the search field.
-        Drupal.coffee.results.html(Drupal.coffee.resultsPlaceholder.children()).find('a:first').addClass('focus');
-      }
-    });
-  });
-
-  Drupal.coffee.open = function () {
-    Drupal.coffee.form.show();
-    Drupal.coffee.bg.show();
-    Drupal.coffee.field.focus();
-  };
-
-  Drupal.coffee.close = function () {
-    Drupal.coffee.field.val('');
-    Drupal.coffee.results.empty();
-    Drupal.coffee.form.hide();
-    Drupal.coffee.bg.hide();
-  };
-
-  /**
-   * Move the focus to the previous or next search result.
-   *
-   * @param direction
-   *   "prev" or "next".
-   * @param activeElement
-   *   jQuery object for the element to use as starting point.
-   */
-  Drupal.coffee.move = function (direction, activeElement) {
   
-    // From the search field: jump to the first (next) or last (prev) result.
-    // Skip the first result if it already has the fake focus class.
-    if (activeElement[0] === Drupal.coffee.field[0]) {
-      Drupal.coffee.results.find( direction === 'prev' ? 'a:last' : (Drupal.coffee.results.find('a:first').hasClass('focus') ? 'li:nth-child(2) a' : 'a:first') ).focus();
-    }
-  
-    // Jump to the last result if prev is used on the first, and the other way around.
-    else if (activeElement[0] === Drupal.coffee.results.find(direction === 'prev' ? 'a:first' : 'a:last')[0]) {
-      Drupal.coffee.results.find( direction === 'prev' ? 'a:last' : 'a:first' ).focus();
-    }
-  
-    // Assuming we're on a result link.
-    else if (direction === 'prev') {
-      activeElement.parent().prev().find('a').focus();
-    }
-    else {
-      activeElement.parent().next().find('a').focus();
-    }
-  };
-
-
   Drupal.behaviors.coffee = {
     attach: function () {
       $('body').once('coffee', function () {
@@ -146,6 +64,87 @@
           return false;
         });
       });
+    }
+  };
+
+  Drupal.coffee = Drupal.coffee || {};
+
+  Drupal.coffee.label = $('<label for="coffee-q" class="element-invisible" />').text(Drupal.t('Query'));
+
+  Drupal.coffee.field = $('<input id="coffee-q" type="text" autocomplete="off" />').keyup(function () {
+    Drupal.coffee.resultsPlaceholder.empty();
+
+    $.getJSON(Drupal.settings.basePath + 'admin/coffee/result/' + Drupal.coffee.field.val(), function (data) {
+      if (data) {
+        $.each(data, function (key, value) {
+          var description = $('<small class="description" />').text(value.path);
+          $('<a />').text(value.title)
+            .attr('href', Drupal.settings.basePath + value.path)
+            .append(description)
+            .appendTo(Drupal.coffee.resultsPlaceholder)
+            .wrap('<li />');
+        });
+
+        // Highlight the first result as if it were focused, as a visual hint for
+        // what will happen when the enter key is used in the search field.
+        Drupal.coffee.results.html(Drupal.coffee.resultsPlaceholder.children()).find('a:first').addClass('focus');
+      }
+    });
+  });
+
+  // Instead of appending results one by one, we put them in a placeholder element
+  // first and then append them all at once to prevent flickering while typing.
+
+  Drupal.coffee.results = $('<ol id="coffee-results" />');
+
+  Drupal.coffee.resultsPlaceholder = $('<ol />');
+
+  Drupal.coffee.form = $('<form id="coffee-form" />');
+
+  Drupal.coffee.bg = $('<div id="coffee-bg" />').click(function () {
+    Drupal.coffee.close();
+  });
+
+  Drupal.coffee.open = function () {
+    Drupal.coffee.form.show();
+    Drupal.coffee.bg.show();
+    Drupal.coffee.field.focus();
+  };
+
+  Drupal.coffee.close = function () {
+    Drupal.coffee.field.val('');
+    Drupal.coffee.results.empty();
+    Drupal.coffee.form.hide();
+    Drupal.coffee.bg.hide();
+  };
+
+  /**
+   * Move the focus to the previous or next search result.
+   *
+   * @param direction
+   *   "prev" or "next".
+   * @param activeElement
+   *   jQuery object for the element to use as starting point.
+   */
+  Drupal.coffee.move = function (direction, activeElement) {
+  
+    // From the search field: jump to the first (next) or last (prev) result.
+    // Skip the first result if it already has the fake focus class.
+    if (activeElement[0] === Drupal.coffee.field[0]) {
+      Drupal.coffee.results.find( direction === 'prev' ? 'a:last' : (Drupal.coffee.results.find('a:first').hasClass('focus') ? 'li:nth-child(2) a' : 'a:first') ).focus();
+    }
+  
+    // Jump to the last result if prev is used on the first, and the other way around.
+    else if (activeElement[0] === Drupal.coffee.results.find(direction === 'prev' ? 'a:first' : 'a:last')[0]) {
+      Drupal.coffee.results.find( direction === 'prev' ? 'a:last' : 'a:first' ).focus();
+    }
+  
+    // Assuming we're on a result link.
+    else if (direction === 'prev') {
+      activeElement.parent().prev().find('a').focus();
+    }
+    else {
+      activeElement.parent().next().find('a').focus();
     }
   };
 
